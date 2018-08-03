fcbb5dea05fcd340f38e347e693c87a6508e49ac
Cristián Donoso <cristiandonosoc@gmail.com>
2018-08-02 23:33:42 -0700

    Initial initializations with Vulkan

--- src/vulkan_test.cc
+++ src/vulkan_test.cc
@@ -2,20 +2,17 @@
 // This code has a BSD license. See LICENSE.
 
 #include <stdio.h>
+#include <windows.h>
 
+#include <memory>
+#include <vector>
 
 #define SDL_MAIN_HANDLED
 #include <SDL2/sdl.h>
+#include <SDL2/SDL_vulkan.h>
 #include <vulkan/vulkan.h>
 
 int main() {
-
-  uint32_t extension_count = 0;
-  vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, nullptr);
-
-  printf("Hello Vulkan!\n");
-  printf("Extensions supported: %d\n", extension_count);
-
   // Setup SDL2
   if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
     printf("Error loading SDL: %s\n", SDL_GetError());
@@ -26,6 +23,61 @@ int main() {
   printf("Information from SDL\n");
   printf("Amount of displays: %d\n", SDL_GetNumVideoDisplays());
 
+  SDL_Window *window = SDL_CreateWindow("Example", SDL_WINDOWPOS_UNDEFINED,
+                                        SDL_WINDOWPOS_UNDEFINED, 640, 480,
+                                        SDL_WINDOW_SHOWN);
+  if (!window) {
+    printf("Error creating window: %s\n", SDL_GetError());
+    return 1;
+  }
+
+  // Vulkan application info
+  VkApplicationInfo app_info = {};
+  app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  app_info.pApplicationName = "Vulkan Test";
+  app_info.applicationVersion = VK_MAKE_VERSION(0, 0, 1);
+  app_info.pEngineName = "Warhol";
+  app_info.engineVersion = VK_MAKE_VERSION(0, 0, 1);
+  app_info.apiVersion = VK_API_VERSION_1_1;
+
+  // Setup extensions.
+  uint32_t extension_count;
+  SDL_Vulkan_GetInstanceExtensions(window, &extension_count, NULL);
+  std::vector<const char*> extensions(extension_count);
+  SDL_Vulkan_GetInstanceExtensions(window, &extension_count, extensions.data());
+
+  VkInstanceCreateInfo create_info = {};
+  create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  create_info.pApplicationInfo = &app_info;
+  create_info.enabledExtensionCount = extension_count;
+  create_info.ppEnabledExtensionNames = extensions.data();
+  create_info.enabledLayerCount = 0;
+
+  printf("SDL extensions (count: %d):\n", extension_count);
+  for (size_t i = 0; i < extension_count; i++)
+    printf("- %s\n", extensions[i]);
+  printf("\n");
+
+  VkInstance instance;
+  VkResult result = vkCreateInstance(&create_info, nullptr, &instance);
+  if (result != VK_SUCCESS) {
+    printf("Error creating vulkan instance\n");
+    return 1;
+  }
+
+  // We get all the extensions.
+  uint32_t count;
+  vkEnumerateInstanceExtensionProperties(nullptr, &count, nullptr);
+  std::vector<VkExtensionProperties> ext(count);
+  vkEnumerateInstanceExtensionProperties(nullptr, &count, ext.data());
+
+  printf("Available extensions:\n");
+  for (size_t i = 0; i < count; i++) {
+    printf("- %s\n", ext[i].extensionName);
+  }
+
+
+  SDL_Quit();
   return 0;
 }
 
