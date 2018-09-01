deb75c257b0743256e1b125039fd1c0f7d2d0006
Cristián Donoso <cristiandonosoc@gmail.com>
2018-09-01 13:40:50 -0700

    Physical Device management

--- src/vulkan_context.cc
+++ src/vulkan_context.cc
@@ -34,8 +34,9 @@ VulkanContext::~VulkanContext() {
     }
   }
 
-  if (swap_chain.handle != VK_NULL_HANDLE)
-    vkDestroySwapchainKHR(logical_device.handle, swap_chain.handle, nullptr);
+  if (physical_device.swap_chain.handle != VK_NULL_HANDLE)
+    vkDestroySwapchainKHR(logical_device.handle,
+                          physical_device.swap_chain.handle, nullptr);
 
   if (logical_device.handle != VK_NULL_HANDLE)
     vkDestroyDevice(logical_device.handle, nullptr);
@@ -56,15 +57,24 @@ VulkanContext::~VulkanContext() {
 
 // Declare the stages
 namespace {
-Status SetupInstance(VulkanContext*);
+Status SetupInstance(SDL_Window*, VulkanContext*);
 Status SetupDebugMessenger(VulkanContext*);
-
+Status SetupPhysicalDevice(VulkanContext*);
+Status SetupSurface(SDL_Window*, VulkanContext*);
 
 // Utils -----------------------------------------------------------------------
 // Adds all the required extensions from SDL and beyond.
 Status AddInstanceExtensions(SDL_Window*, VulkanContext*);
 Status AddInstanceValidationLayers(VulkanContext*);
 Status CheckRequiredLayers(const std::vector<const char*>&);
+
+VulkanContext::PhysicalDevice::SwapChain
+GetSwapChainProperties(const VulkanContext&,
+                       const VulkanContext::PhysicalDevice&);
+bool
+IsSuitablePhysicalDevice(const VulkanContext::PhysicalDevice&,
+                         const std::vector<const char*>& extensions);
+
 }  // namespace
 
 #define RETURN_IF_ERROR(status, call) \
@@ -76,10 +86,10 @@ Status
 InitVulkanContext(SDL_Window* window, VulkanContext* context) {
   Status status;
   // Instance.
-  RETURN_IF_ERROR(status, AddInstanceExtensions(window, context));
-  RETURN_IF_ERROR(status, AddInstanceValidationLayers(context));
-  RETURN_IF_ERROR(status, SetupInstance(context));
+  RETURN_IF_ERROR(status, SetupInstance(window, context));
   RETURN_IF_ERROR(status, SetupDebugMessenger(context));
+  RETURN_IF_ERROR(status, SetupSurface(window, context));
+  RETURN_IF_ERROR(status, SetupPhysicalDevice(context));
 
   return Status::Ok();
 }
@@ -87,7 +97,12 @@ InitVulkanContext(SDL_Window* window, VulkanContext* context) {
 // Stages implementation.
 namespace {
 
-Status SetupInstance(VulkanContext* context) {
+Status SetupInstance(SDL_Window* window, VulkanContext* context) {
+  // TODO: The extensions and validation layers should be exposed.
+  Status status;
+  RETURN_IF_ERROR(status, AddInstanceExtensions(window, context));
+  RETURN_IF_ERROR(status, AddInstanceValidationLayers(context));
+
   // Vulkan application info.
   VkApplicationInfo app_info = {};
   app_info.sType              = VK_STRUCTURE_TYPE_APPLICATION_INFO;
@@ -120,7 +135,8 @@ Status SetupInstance(VulkanContext* context) {
   return Status::Ok();
 }
 
-Status SetupDebugMessenger(VulkanContext* context) {
+Status
+SetupDebugMessenger(VulkanContext* context) {
   VkDebugUtilsMessengerCreateInfoEXT messenger_info = {};
   messenger_info.sType =
       VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
@@ -135,9 +151,90 @@ Status SetupDebugMessenger(VulkanContext* context) {
   messenger_info.pUserData = nullptr;
 
   Status status =
-      CreateDebugUtilsMessengerEXT(context->instance.handle, &messenger_info, nullptr,
-                                   &context->debug_messenger);
+      CreateDebugUtilsMessengerEXT(context->instance.handle, &messenger_info,
+                                   nullptr, &context->debug_messenger);
   return status;
+}
+
+Status
+SetupSurface(SDL_Window* window, VulkanContext* context) {
+  if (!SDL_Vulkan_CreateSurface(window, context->instance.handle,
+                                &context->surface)) {
+    return Status("Could not create surface: %s\n", SDL_GetError());
+  }
+  return Status::Ok();
+}
+
+Status
+SetupPhysicalDevice(VulkanContext* context) {
+  std::vector<VkPhysicalDevice> device_handles;
+  VK_GET_PROPERTIES(vkEnumeratePhysicalDevices, context->instance.handle,
+                    device_handles);
+
+  // Enumarate device properties.
+  std::vector<VulkanContext::PhysicalDevice> devices;
+  devices.reserve(device_handles.size());
+  printf("Found %zu physical devices:\n", device_handles.size());
+  for (auto& device_handle : device_handles) {
+    VulkanContext::PhysicalDevice device;
+    device.handle = device_handle;
+    vkGetPhysicalDeviceProperties(device.handle, &device.properties);
+    vkGetPhysicalDeviceFeatures(device.handle, &device.features);
+
+    printf("--------------------------------------------\n");
+    printf("Device Name: %s\n", device.properties.deviceName);
+    printf("Type: %s\n", VulkanEnumToString(device.properties.deviceType));
+    printf("API Version: %u\n", device.properties.apiVersion);
+    printf("Driver Version: %u\n", device.properties.driverVersion);
+    printf("Vendor ID: %x\n", device.properties.vendorID);
+    printf("Device ID: %x\n", device.properties.deviceID);
+    fflush(stdout);
+
+    // We setup the queue families data for each device.
+    VK_GET_PROPERTIES(vkGetPhysicalDeviceQueueFamilyProperties, device.handle,
+                      (device.qf_properties));
+
+    // Get the queues
+    int i = 0;
+    for (auto& qfp : device.qf_properties) {
+      if (qfp.queueCount == 0)
+        continue;
+
+      // Get the graphical queue.
+      if (qfp.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+        device.graphics_queue_index = i;
+
+      // Get the present queue.
+      VkBool32 present_support = false;
+      vkGetPhysicalDeviceSurfaceSupportKHR(device.handle, i, context->surface,
+                                           &present_support);
+      if (present_support)
+        device.present_queue_index = i;
+
+      i++;
+    }
+
+    device.swap_chain = GetSwapChainProperties(*context, device);
+
+    // We check if this is a suitable device
+    std::vector<const char*> extensions;
+    extensions.push_back(VK_KHR_SWAPCHAIN_EXTENSION_NAME);
+    if (IsSuitablePhysicalDevice(device, extensions)) {
+      printf("Device \"%s\" is not suitable\n", device.properties.deviceName);
+      devices.push_back(std::move(device));
+    }
+  }
+
+  if (devices.empty())
+    return Status("No suitable device found");
+
+  // TODO: Find a better heuristic to get the device.
+  //       For now we get the first.
+  context->physical_device = devices.front();
+  printf("Selected device \"%s\"\n",
+         context->physical_device.properties.deviceName);
+
+  return Status::Ok();
 }
 
 
@@ -192,10 +289,83 @@ CheckRequiredLayers(const std::vector<const char*>& requested_layers) {
   return Status::Ok();
 };
 
+VulkanContext::PhysicalDevice::SwapChain
+GetSwapChainProperties(const VulkanContext& context,
+                       const VulkanContext::PhysicalDevice& device) {
+  // Setup the swap chain.
+  VulkanContext::PhysicalDevice::SwapChain swap_chain;
+  vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device.handle, context.surface,
+                                            &swap_chain.capabilites);
+  uint32_t format_count;
+  vkGetPhysicalDeviceSurfaceFormatsKHR(device.handle, context.surface,
+                                       &format_count, nullptr);
+  swap_chain.formats.resize(format_count);
+  vkGetPhysicalDeviceSurfaceFormatsKHR(
+      device.handle, context.surface, &format_count, swap_chain.formats.data());
+
+  uint32_t present_mode_count;
+  vkGetPhysicalDeviceSurfacePresentModesKHR(device.handle, context.surface,
+                                            &present_mode_count, nullptr);
+
+  swap_chain.present_modes.resize(present_mode_count);
+  vkGetPhysicalDeviceSurfacePresentModesKHR(device.handle, context.surface,
+                                            &present_mode_count,
+                                            swap_chain.present_modes.data());
+
+  return swap_chain;
+}
+
+
+
+bool
+IsSuitablePhysicalDevice(const VulkanContext::PhysicalDevice& device,
+                         const std::vector<const char*>& extensions) {
+  // Queues.
+  if (device.graphics_queue_index < 0 ||
+      device.present_queue_index < 0)
+    return false;
+
+
+  if (extensions.empty())
+    return true;
+
+  // Get the the extensions the physical device actually offers.
+  uint32_t extension_count = 0;
+  vkEnumerateDeviceExtensionProperties(device.handle,
+                                       nullptr,
+                                       &extension_count,
+                                       nullptr);
+  std::vector<VkExtensionProperties> available_extensions;
+  available_extensions.resize(extension_count);
+  vkEnumerateDeviceExtensionProperties(device.handle,
+                                       nullptr,
+                                       &extension_count,
+                                       available_extensions.data());
+
+  // All extensions should be present.
+  for (const char* extension : extensions) {
+    bool found = false;
+    for (const auto& available_extension : available_extensions) {
+      if (strcmp(available_extension.extensionName,
+                 extension) == 0) {
+        found = true;
+        break;
+      }
+    }
+
+    if (!found)
+      return false;
+  }
+
+  // Swap chain properties.
+  if (device.swap_chain.formats.empty() ||
+      device.swap_chain.present_modes.empty()) {
+    return false;
+  }
+
+  return true;
+}
+
 }  // namespace
-
-
-
-
 
 }  // namespace warhol
--- src/vulkan_context.h
+++ src/vulkan_context.h
@@ -24,39 +24,38 @@ struct VulkanContext {
   } instance;
 
   // Physical Device
-  struct {
+  struct PhysicalDevice {
     VkPhysicalDevice handle = VK_NULL_HANDLE;
     VkPhysicalDeviceProperties properties;
     VkPhysicalDeviceFeatures features;
 
+    // Queues
     int graphics_queue_index = -1;
     int present_queue_index = -1;
     std::vector<VkQueueFamilyProperties> qf_properties;
+
+    struct SwapChain {
+      std::vector<VkSurfaceFormatKHR> formats;
+      std::vector<VkPresentModeKHR> present_modes;
+
+      VkSwapchainKHR handle = VK_NULL_HANDLE;
+
+      VkSurfaceCapabilitiesKHR capabilites;
+      VkSurfaceFormatKHR format;
+      VkPresentModeKHR present_mode;
+      VkExtent2D extent;
+    } swap_chain;
   } physical_device;
 
   VkDebugUtilsMessengerEXT debug_messenger = VK_NULL_HANDLE;
   VkSurfaceKHR surface = VK_NULL_HANDLE;
 
   // Logical Device
-  struct {
+  struct LogicalDevice {
     VkDevice handle = VK_NULL_HANDLE;
     VkQueue graphics_queue = VK_NULL_HANDLE;
     VkQueue present_queue = VK_NULL_HANDLE;
-
   } logical_device;
-
-  // Swap Chain
-  struct {
-    /* std::vector<VkSurfaceFormatKHR> formats; */
-    /* std::vector<VkPresentModeKHR> present_modes; */
-
-    VkSwapchainKHR handle = VK_NULL_HANDLE;
-
-    VkSurfaceCapabilitiesKHR capabilites;
-    VkSurfaceFormatKHR format;
-    VkPresentModeKHR present_mode;
-    VkExtent2D extent;
-  } swap_chain;
 
   std::vector<VkImage> images;
   std::vector<VkImageView> image_views;
