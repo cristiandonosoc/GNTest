0e79b69d4c4324db7c135cdbfeb2e9bfc7726664
Cristián Donoso <cristiandonosoc@gmail.com>
2018-10-21 11:42:42 -0700

    Added vec math

---
+++ src/math/vec.h
@@ -0,0 +1,14 @@
+// Copyright 2018, Cristián Donoso.
+// This code has a BSD license. See LICENSE.
+
+#pragma once
+
+namespace warhol {
+
+template <typename T>
+struct Vec2 {
+  T x;
+  T y;
+};
+
+}  // namespace warhol
