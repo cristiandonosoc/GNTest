7d60628c753ec233916c2fc1851aed5523492c16
Cristián Donoso <donosoc@google.com>
2019-01-11 08:20:26 -0800

    Test of git lfs

---
+++ third_party/tiny_obj_loader/implementation.cc
@@ -0,0 +1,6 @@
+// Author: 2018, Cristián Donoso.
+// This is glue for third party Tiny OBJ Loader code.
+// It is licensed with the same license as any other Tiny OBJ source code.
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "tiny_obj_loader.h"
